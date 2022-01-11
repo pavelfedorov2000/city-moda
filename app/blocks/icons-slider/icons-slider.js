63234946adba21cdf095fe49deece5acfd25ae7d
Pavel Fedorov <p.fedorov@imedia.by>
2022-01-11 18:37:39 +0300

    SE-13 icons-slider, actual, brands done

--- app/blocks/icons-slider/icons-slider.js
+++ app/blocks/icons-slider/icons-slider.js
@@ -1,7 +1,13 @@
-app.yourScriptName = {
-  name: 'yourScriptName',
-  description: 'your script description',
-  init() {
-    // your code
-  },
+app.iconsSlider = {
+	name: 'iconsSlider',
+	description: 'your script description',
+	init() {
+		// ================ ICONS-SLIDER ============== //
+		new Swiper('.icons-slider__swiper', {
+			slidesPerView: 'auto',
+			freeMode: true,
+			//freeModeMomentum: true,
+			//spaceBetween: 52,
+		});
+	},
 };
--- app/scripts/init.js
+++ app/scripts/init.js
@@ -2,6 +2,7 @@
 //new WOW().init();
 app.burger.init();
 app.dropMenu.init();
+app.iconsSlider.init();
 /* document.querySelector('.page-up').addEventListener('click', () => {
 	window.scrollTo({
 		top: 0,
