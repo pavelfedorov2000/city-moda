224fcc980c9c3e5acff0a389ae69e42e64b64384
Pavel Fedorov <p.fedorov@imedia.by>
2022-04-27 17:45:02 +0300

    27.04 fixes

--- app/blocks/basket-total/basket-total.js
+++ app/blocks/basket-total/basket-total.js
@@ -3,28 +3,18 @@ app.basketTotal = {
   description: 'your script description',
   init() {
 
-    const headerHeight = document.querySelector('.header').getBoundingClientRect().height;
-    const basketTotalFixedTable = document.querySelector('.basket-total');
-    const basketPageBody = document.querySelector('.basket-page__body');
-    const basketPageBodyHeight = basketPageBody.getBoundingClientRect().height;
-    const breadcrumbsHeight = document.querySelector('.breadcrumbs').getBoundingClientRect().height;
-    const h1 = document.querySelector('h1');
-
     function fixBasketTable() {
-      window.addEventListener('scroll', () => {
-        if (window.innerWidth >= 1025 && window.scrollY > headerHeight + breadcrumbsHeight + h1.getBoundingClientRect().height && window.scrollY <= basketPageBodyHeight - headerHeight) {
-          basketTotalFixedTable.classList.add('fixed');
-          basketPageBody.classList.add('scroll-grid');
-          basketTotalFixedTable.style.top = '30px';
-        } else {
-          basketTotalFixedTable.classList.remove('fixed');
-          basketPageBody.classList.remove('scroll-grid');
-          basketTotalFixedTable.style.top = `${headerHeight + basketPageBody.offsetTop - window.scrollY}px`;
-        }
-      });
+      if (window.innerWidth > 1024) {
+        new StickySidebar('.basket-total', {
+          topSpacing: 20,
+          bottomSpacing: 20,
+          containerSelector: '.basket-page__body',
+          innerWrapperSelector: '.basket-total__inner',
+          //scrollContainer: '#main-viewport'
+        });
+      }
     }
     fixBasketTable();
-
     window.addEventListener('resize', () => {
       fixBasketTable();
     });
