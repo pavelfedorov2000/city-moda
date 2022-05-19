0d38913a9fa779336edcb6f36f814d50519e8a49
Pavel Fedorov <p.fedorov@imedia.by>
2022-05-19 18:48:58 +0300

    19.05.22

--- app/blocks/checkout/checkout.js
+++ app/blocks/checkout/checkout.js
@@ -1,12 +1,12 @@
 app.checkout = {
-	name: 'checkout',
-	description: 'your script description',
-	init() {
-		$('.checkout-radio').on('click', function () {
-			$(this).siblings().removeClass('checkout-radio--checked');
-			$(this).addClass('checkout-radio--checked');
-			$(this).siblings().find('.checkout-radio__hidden').slideUp('300');
-			$(this).find('.checkout-radio__hidden').slideDown('300');
-		});
-	},
+  name: 'checkout',
+  description: 'your script description',
+  init() {
+    $('.checkout-radio').on('click', function () {
+      $(this).siblings().removeClass('checkout-radio--checked');
+      $(this).addClass('checkout-radio--checked');
+      $(this).siblings().find('.checkout-radio__hidden').slideUp('300');
+      $(this).find('.checkout-radio__hidden').slideDown('300');
+    });
+  },
 };
